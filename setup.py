aef2c2ff1aa6f2f247c290f65abb29ca280b08bf
jdhuang <jdhuang@mines.edu>
2023-01-22 12:35:57 -0700

    Updated requirements

--- setup.py
+++ setup.py
@@ -5,24 +5,21 @@ from setuptools import setup, find_packages
 
 setup(name='hybrid-drt',
       version='0.1',
-      description='A Python package for analyzing square wave impedance data',
+      description='A Python package for probabilistic electrochemical analysis',
       # long_description=long_description,
       # long_description_content_type="text/markdown",
-      # url='https://github.com/jdhuang-csm/bayes-drt',
+      url='https://github.com/jdhuang-csm/hybrid-drt',
       author='Jake Huang',
       author_email='jdhuang@mines.edu',
       license='BSD 3-clause',
       packages=find_packages(),
       install_requires=[
           'numpy',
-          'scipy',
           'pandas',
+          'scipy',
           'cvxopt',
-          # 'cmdstanpy',
           'matplotlib',
-          'mitlef',
-          'pytorch',
-          'gpytorch'
+          'scikit-learn',
       ],
       include_package_data=True
       )
