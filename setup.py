af9e401cf783a5841d2db4e8d0085dbee938f74d
Jake Huang <jdhuang@mines.edu>
2026-01-08 12:29:29 +0100

    Update required numpy version

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from setuptools import setup, find_packages
 
-# with open("README.md", "r",encoding="utf8") as fh:
-#     long_description = fh.read()
 
 setup(name='hybrid-drt',
       version='0.1',
       description='A Python package for probabilistic electrochemical analysis',
-      # long_description=long_description,
-      # long_description_content_type="text/markdown",
       url='https://github.com/jdhuang-csm/hybrid-drt',
       author='Jake Huang',
       author_email='jdhuang@mines.edu',
       license='BSD 3-clause',
       packages=find_packages(),
       install_requires=[
-          'numpy',
+          'numpy >= 2.0.0',
           'pandas',
           'scipy',
           'cvxopt',
